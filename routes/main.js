14371c2180f617caa4e20da9a515db78525a98b0
Prouser123 <jcahill773@gmail.com>
2019-04-09 11:46:25 +0100

    show commit rev in main command footer

--- routes/main.js
+++ routes/main.js
@@ -5,11 +5,17 @@ const { prefix } = require("../index");
 module.exports = function(client) {
   client.on("message", msg => {
     if (msg.content == prefix) {
+      // get git revision
+      revision = require("child_process")
+        .execSync("git rev-parse --short HEAD")
+        .toString()
+        .trim();
+      // create embed
       embed = new Discord.RichEmbed()
         .setColor(3447003)
         .setAuthor("Commands", client.user.avatarURL)
         // Set footer text, icon and timestamp
-        .setFooter(`Xbox Live | ${prefix}`, client.user.avatarURL)
+        .setFooter(`Xbox Live | ${prefix} (${revision})`, client.user.avatarURL)
         .setTimestamp()
         // Add fields
         .addField("!xbox search <gamertag>", "find a user by their gamertag")
