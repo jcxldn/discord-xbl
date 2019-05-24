c7619a33e9efb36793e14ef0320379d6e0a3d3c8
Prouser123 <jcahill773@gmail.com>
2019-05-24 23:51:47 +0100

    rename !xbox forza to !xbox stats and add many more titles

--- routes/stats.js
+++ routes/stats.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
 const request = require("request");
-const console = require("prefix-logger")("route.forza");
+const console = require("prefix-logger")("route.stats");
 const helpers = require("../helpers");
-const { prefix } = require("../index");
 
 const gameMap = {
   motorsport5: 2067126551,
@@ -10,25 +9,38 @@ const gameMap = {
   motorsport7: 1999574456,
   horizon2: 446059611,
   horizon3: 1289871275,
-  horizon4: 1985701699
+  horizon4: 1985701699,
+  halomcc: 1144039928,
+  shadowtombraider: 913783821,
+  risetombraider: 434412541,
+  metroexodus: 1045960524,
+  titanfall2: 1145574011,
+  titanfall: 1292135256,
+  battlefield1: 1386529057,
+  battlefield4: 1484511986,
+  deusex: 1650442817,
+  battlefront: 1681220621,
+  battlefront2: 833960563,
+  pvzgw: 860666361,
+  pvzgw2: 728655825
 };
 
 module.exports = function(client) {
   client.on("message", msg => {
-    if (msg.content.startsWith(helpers.formatCommand("forza"))) {
+    if (msg.content.startsWith(helpers.formatCommand("stats"))) {
       const split = msg.content.split(" ");
       const game = split[2];
       const gamertag = msg.content.split(
-        helpers.formatCommand("forza") + ` ${game} `
+        helpers.formatCommand("stats") + ` ${game} `
       )[1];
 
       // get the names of all supported games
       gameMapKeys = Object.keys(gameMap);
 
-      // if message was just '!xbox forza'
-      if (msg.content == helpers.formatCommand("forza")) {
+      // if message was just '!xbox stats'
+      if (msg.content == helpers.formatCommand("stats")) {
         msg.channel.send(
-          "Usage:  `!xbox forza <game> <gamertag>`\nGame Types:  `" +
+          "Usage:  `!xbox stats <game> <gamertag>`\nGame Types:  `" +
             Object.keys(gameMap)
               .toString()
               .replace(/,/g, ", ") +
@@ -53,7 +65,7 @@ module.exports = function(client) {
       // if message did not include a gamertag
       if (gamertag == undefined) {
         msg.channel.send(
-          "Please provide a gamertag. `!xbox forza " +
+          "Please provide a gamertag. `!xbox stats " +
             gameMapKeys[Object.keys(gameMap).indexOf(game)] +
             " <gamertag>`"
         );
@@ -64,7 +76,7 @@ module.exports = function(client) {
         gameMap[game]
       }`;
 
-      helpers.debug.level1(`url.1: ${url}`, "route.forza");
+      helpers.debug.level1(`url.1: ${url}`, "route.stats");
 
       request(url, function(error, response, body) {
         // Check the HTTP Response Code.
@@ -85,15 +97,28 @@ module.exports = function(client) {
         // Add stats to array
         const stats = [];
 
+        // KD (Halo:MCC)
+        let kd = {
+          k: undefined,
+          d: undefined
+        };
+
         // Main stat location
         json.groups.map(function(groups) {
           groups.statlistscollection.map(function(statcollection) {
             statcollection.stats.map(function(stat) {
+              // Remove empty stat values
+              if (stat.value == undefined) return;
+              // Push the stat to the array
               stats.push({
                 name: stat.name,
                 displayName: stat.properties.DisplayName,
                 value: stat.value
               });
+
+              // KD (Halo:MCC)
+              if (stat.name == "TotalMultiplayerDeaths") kd.d = stat.value;
+              if (stat.name == "TotalMultiplayerKills") kd.k = stat.value;
             });
           });
         });
@@ -101,6 +126,9 @@ module.exports = function(client) {
         // Alternate stat location - usually Minutes Played
         json.statlistscollection.map(function(statcollection) {
           statcollection.stats.map(function(stat) {
+            // Remove empty stat values
+            if (stat.value == undefined) return;
+            // Push the stat to the array
             stats.push({
               name: stat.name,
               displayName: stat.properties.displayName,
@@ -109,7 +137,16 @@ module.exports = function(client) {
           });
         });
 
-        helpers.debug.level2(JSON.stringify(stats), "route.forza.stats");
+        // KD (Halo:MCC)
+        if (kd.d != undefined && kd.k != undefined) {
+          stats.push({
+            //name: "Multiplayer KD",
+            displayName: "Multiplayer K/D Ratio",
+            value: (kd.k / kd.d).toString().substring(0, 4)
+          });
+        }
+
+        helpers.debug.level2(JSON.stringify(stats), "route.stats.res");
 
         // If the first stat value is undefined, the user likely hasn't played the game
         if (stats[0].value == undefined) {
@@ -128,14 +165,14 @@ module.exports = function(client) {
 
         // Get titleInfo (such as photos)
         const url = `https://xbl-api.prouser123.me/titleinfo/${gameMap[game]}`;
-        helpers.debug.level1(`url.2: ${url}`, "route.forza");
+        helpers.debug.level1(`url.2: ${url}`, "route.stats");
         request(url, function(error, response, body) {
           const json2 = JSON.parse(response.body);
           embed.setThumbnail(json2.titles[0].displayImage);
 
           // Get user's profile pic
           const url = `https://xbl-api.prouser123.me/profile/gamertag/${gamertag}`;
-          helpers.debug.level1(`url.3: ${url}`, "route.forza");
+          helpers.debug.level1(`url.3: ${url}`, "route.stats");
           request(url, function(error, response, body) {
             embed.setAuthor(
               `${gamertag}'s ${json2.titles[0].name} Stats`,
