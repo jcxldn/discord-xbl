6821e98a97de970582688412ede402ec294d3559
James Cahill <jcahill773@gmail.com>
2019-10-07 18:29:53 +0100

    Colors ASYNC PoC

--- routes/colors.js
+++ routes/colors.js
@@ -1,11 +1,11 @@
 const sharp = require("sharp");
+const rp = require("request-promise");
 
 const helpers = require("../helpers");
 const console = require("prefix-logger")("route.colors");
-const request = require("request");
 
 module.exports = function(client) {
-  client.on("message", msg => {
+  client.on("message", async msg => {
     if (msg.content.startsWith(helpers.formatCommand("colors"))) {
       const searchTag = msg.content.split("colors ")[1];
 
@@ -13,68 +13,48 @@ module.exports = function(client) {
         searchTag
       )}`;
       helpers.debug.level1(`url: ${url}`, "route.search");
-      request({ url }, function(error, response, body) {
-        // Check the HTTP Response Code.
-        if (response.statusCode !== 200) {
-          msg.reply("Sorry, I coudn't get your request from the API.");
-          console.warn(`HTTP Response ${response.statusCode}`);
-          return;
-        }
 
-        // Check the body for error codes from API.
-        if (!helpers.responseFormatter.checkBody(body)) {
-          msg.reply("We coudn't find that gamertag. Please try again.");
-          return;
-        }
+      const user = await rp({
+        url: `https://xbl-api.prouser123.me/profile/gamertag/${encodeURIComponent(
+          searchTag
+        )}`
+      }).catch(err => {
+        msg.reply("Sorry, I coudn't get your request from the API.");
+        console.error(err);
+        return;
+      });
 
-        // Format data
-        //const data = helpers.responseFormatter.profileUsers(body);
+      const colasset = await rp({
+        url: JSON.parse(user).profileUsers[0].settings[9].value
+      }).catch(err => {
+        msg.reply("Sorry, I coudn't get your request from the API.");
+        console.error(err);
+        return;
+      });
 
-        var v = JSON.parse(body).profileUsers[0].settings[9].value;
+      const svg = await rp({
+        url: `https://xbl-api.prouser123.me/usercolors/define/${encodeURIComponent(
+          JSON.parse(colasset).primaryColor
+        )}/${encodeURIComponent(
+          JSON.parse(colasset).secondaryColor
+        )}/${encodeURIComponent(JSON.parse(colasset).tertiaryColor)}`
+      }).catch(err => {
+        msg.reply("Sorry, I coudn't get your request from the API.");
+        console.error(err);
+        return;
+      });
 
-        request(
+      const png = await sharp(Buffer.from(svg))
+        .png()
+        .toBuffer();
+
+      msg.channel.send({
+        files: [
           {
-            url:
-              "http://dlassets.xboxlive.com/public/content/ppl/colors/00003.json"
-          },
-          function(error, response, body) {
-            console.warn(`HTTP Response ${response.statusCode}`);
-            // Check the HTTP Response Code.
-            if (response.statusCode !== 200) {
-              msg.reply("Sorry, I coudn't get your request from the API.");
-              console.warn(`HTTP Response ${response.statusCode}`);
-              return;
-            }
-
-            const val = JSON.parse(body);
-
-            request(
-              {
-                url: `http://localhost:3000/usercolors/define/${encodeURIComponent(
-                  val.primaryColor
-                )}/${encodeURIComponent(
-                  val.secondaryColor
-                )}/${encodeURIComponent(val.tertiaryColor)}`
-              },
-              function(error, response, body) {
-                console.log(body);
-                sharp(Buffer.from(body))
-                  .png()
-                  .toBuffer()
-                  .then(data =>
-                    msg.channel.send({
-                      files: [
-                        {
-                          name: "image.png",
-                          attachment: data
-                        }
-                      ]
-                    })
-                  );
-              }
-            );
+            name: "colors.png",
+            attachment: png
           }
-        );
+        ]
       });
     }
   });
