6213ff75066624d7c8f4d6bbcf905d8c4582db65
James Cahill <jcahill773@gmail.com>
2019-10-13 18:14:16 +0100

    Remove old response formatters

--- helpers/response-formatter.js
+++ helpers/response-formatter.js
@@ -41,63 +41,6 @@ function checkBody(body) {
   }
 }
 
-const myAccount = body => {
-  /*
-    Missing in this endpoint:
-        - Presence (State)
-        - Presence (Text)
-        - Tenure
-        - Watermarks
-    */
-  const json = JSON.parse(body);
-  var obj = Object.assign({}, responseObj);
-
-  // Set the variables
-  obj.pictureURL = json.profileUsers[0].settings[0].value;
-  obj.gamerscore = json.profileUsers[0].settings[1].value;
-  obj.gamertag = json.profileUsers[0].settings[2].value;
-  obj.subscriptionType = json.profileUsers[0].settings[3].value;
-  obj.reputation = json.profileUsers[0].settings[4].value.split("Player")[0];
-  obj.realName = json.profileUsers[0].settings[6].value;
-  obj.bio = json.profileUsers[0].settings[7].value;
-  obj.location = json.profileUsers[0].settings[8].value;
-
-  // Remove empty values
-  obj._removeEmptyValues();
-
-  // Return the object
-  return obj;
-};
-
-const profileUsers = body => {
-  /*
-    Missing in this endpoint:
-        - Presence (State)
-        - Presence (Text)
-    */
-  const json = JSON.parse(body);
-  var obj = Object.assign({}, responseObj);
-
-  // Set the variables
-  obj.pictureURL = json.profileUsers[0].settings[0].value;
-  obj.gamerscore = json.profileUsers[0].settings[1].value;
-  obj.gamertag = json.profileUsers[0].settings[2].value;
-  obj.subscriptionType = json.profileUsers[0].settings[3].value;
-  obj.reputation = json.profileUsers[0].settings[4].value.split("Player")[0];
-  obj.realName = json.profileUsers[0].settings[6].value;
-  obj.bio = json.profileUsers[0].settings[7].value;
-  if (json.profileUsers[0].settings[8].value != 0)
-    obj.tenure = json.profileUsers[0].settings[8].value;
-  obj.watermarks = json.profileUsers[0].settings[9].value.replace(/\|/g, ", ");
-  obj.location = json.profileUsers[0].settings[10].value;
-
-  // Remove empty values
-  obj._removeEmptyValues();
-
-  // Return the object
-  return obj;
-};
-
 const px_profile = body => {
   /*
       XBL-API.PROUSER123.ME FORMAT
@@ -130,37 +73,7 @@ const px_profile = body => {
   return obj;
 };
 
-const playerSummary = body => {
-  /*
-    Missing in this endpoint:
-        - Subscription Type
-        - Location
-        - Tenure
-        - Watermarks
-    */
-  const json = JSON.parse(body);
-  console.log(json);
-  var obj = Object.assign({}, responseObj);
-  // Set the variables
-  obj.pictureURL = json.people[0].displayPicRaw;
-  obj.gamerscore = json.people[0].gamerScore;
-  obj.gamertag = json.people[0].gamertag;
-  obj.reputation = json.people[0].xboxOneRep.split("Player")[0];
-  obj.realName = json.people[0].realName;
-  obj.presenceState = json.people[0].presenceState;
-  obj.presenceText = json.people[0].presenceText;
-
-  // Remove empty values
-  obj._removeEmptyValues();
-
-  // Return the object
-  return obj;
-};
-
 module.exports = {
   checkBody,
-  myAccount,
-  profileUsers,
-  playerSummary,
   px_profile
 };
