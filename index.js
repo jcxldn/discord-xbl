33761a840802f54c1c038a59c3513d90d70d7e34
James Cahill <jcahill773@gmail.com>
2019-10-13 18:06:49 +0100

    Consolidate env-loader into index and throw nice errors

--- index.js
+++ index.js
@@ -11,9 +11,59 @@ const debugLevel = 2;
 
 module.exports = { prefix, debugLevel };
 
-// Require local files / modules
-require("./env-loader");
-const helpers = require("./helpers");
+// -------------------- Exit Handler --------------------
+
+// To prevent the exit handler from being called multiple times at once
+let isExiting = false;
+
+function exitHandler(options, exitCode) {
+  if (!isExiting) {
+    isExiting = true;
+    console.warn("Cleaning up...", "exit");
+    try {
+      client.destroy();
+      console.warn("Disconnected from Discord.", "exit");
+    } catch (e) {
+      console.log("Discord never connected.", "exit");
+    }
+    process.exit();
+  }
+}
+
+//do something when app is closing
+process.on("exit", exitHandler.bind(null));
+
+//catches ctrl+c event
+process.on("SIGINT", exitHandler.bind(null));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on("SIGUSR1", exitHandler.bind(null));
+process.on("SIGUSR2", exitHandler.bind(null));
+
+//catches uncaught exceptions
+process.on("uncaughtException", function(e) {
+  console.error(e, "uncaughtException");
+  exitHandler();
+});
+
+// -------------------- Environment Variables --------------------
+
+// Attempt to import the universal-dotenv package to load env vars from .env.
+try {
+  require("universal-dotenv");
+  console.log("Using universal-dotenv.");
+} catch (e) {
+  if (e.code !== "MODULE_NOT_FOUND") {
+    console.warn("Coudn't find universal-dotenv.");
+  }
+}
+
+if (process.env.DISCORD_TOKEN === undefined) {
+  console.error("no token: Discord");
+  exitHandler.call(null);
+} else {
+  console.log("found token: Discord");
+}
 
 // Print some constants
 console.log(`Bot prefix: ${colors.green(prefix)}`);
@@ -52,36 +102,3 @@ client.on("guildDelete", function(guild) {
 require("./routes")(client);
 
 client.login(process.env.DISCORD_TOKEN);
-
-// -------------------- Exit Handler --------------------
-// /*
-let isExiting = false;
-
-function exitHandler(options, exitCode) {
-  if (!isExiting) {
-    isExiting = true;
-    //helpers.log("--------------------", "exit");
-    console.warn("Cleaning up...", "exit");
-    client.destroy();
-    console.warn("Disconnected from Discord.", "exit");
-    process.exit();
-  }
-}
-
-//do something when app is closing
-process.on("exit", exitHandler.bind(null));
-
-//catches ctrl+c event
-process.on("SIGINT", exitHandler.bind(null));
-
-// catches "kill pid" (for example: nodemon restart)
-process.on("SIGUSR1", exitHandler.bind(null));
-process.on("SIGUSR2", exitHandler.bind(null));
-
-//catches uncaught exceptions
-process.on("uncaughtException", function(e) {
-  console.error(e, "uncaughtException");
-  exitHandler();
-});
-
-// */
