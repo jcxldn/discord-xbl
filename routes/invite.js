710fe2b3ce79e4fb9c332f4dc6545632ffded3d0
James Cahill <jcahill773@gmail.com>
2019-10-13 18:37:08 +0100

    Update link (final)

--- routes/invite.js
+++ routes/invite.js
@@ -5,7 +5,7 @@ module.exports = function(client) {
   client.on("message", msg => {
     if (msg.content.startsWith(helpers.formatCommand("invite"))) {
       msg.author.send(
-        "Click the link to add me to your server!\nhttps://go.prouser123.me/discord-xbl-invite"
+        "Click the link to add me to your server!\nhttps://jcx.page.link/discord-xbl-invite"
       );
     }
   });
