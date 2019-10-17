d6dde433a668d4de10671b7cd27cf003a1deffbe
James Cahill <jcahill773@gmail.com>
2019-10-17 17:57:25 +0100

    Better error handling in colors route

--- routes/colors.js
+++ routes/colors.js
@@ -8,54 +8,47 @@ module.exports = function(client) {
   client.on("message", async msg => {
     if (msg.content.startsWith(helpers.formatCommand("colors"))) {
       const searchTag = msg.content.split("colors ")[1];
-
-      const url = `https://xbl-api.prouser123.me/profile/gamertag/${encodeURIComponent(
-        searchTag
-      )}`;
-      helpers.debug.level1(`url: ${url}`, "route.search");
-
-      const user = await rp({
-        url: `https://xbl-api.prouser123.me/profile/gamertag/${encodeURIComponent(
+      try {
+        const url = `https://xbl-api.prouser123.me/profile/gamertag/${encodeURIComponent(
           searchTag
-        )}`
-      }).catch(err => {
+        )}`;
+        helpers.debug.level1(`url: ${url}`, "route.search");
+
+        const user = await rp({
+          url: `https://xbl-api.prouser123.me/profile/gamertag/${encodeURIComponent(
+            searchTag
+          )}`
+        });
+
+        const colasset = await rp({
+          url: JSON.parse(user).profileUsers[0].settings[9].value
+        });
+
+        const svg = await rp({
+          url: `https://xbl-api.prouser123.me/usercolors/define/${encodeURIComponent(
+            JSON.parse(colasset).primaryColor
+          )}/${encodeURIComponent(
+            JSON.parse(colasset).secondaryColor
+          )}/${encodeURIComponent(JSON.parse(colasset).tertiaryColor)}`
+        });
+
+        const png = await sharp(Buffer.from(svg))
+          .png()
+          .toBuffer();
+
+        msg.channel.send({
+          files: [
+            {
+              name: "colors.png",
+              attachment: png
+            }
+          ]
+        });
+      } catch (err) {
         msg.reply("Sorry, I coudn't get your request from the API.");
-        console.error(err);
+        console.error(err.message);
         return;
-      });
-
-      const colasset = await rp({
-        url: JSON.parse(user).profileUsers[0].settings[9].value
-      }).catch(err => {
-        msg.reply("Sorry, I coudn't get your request from the API.");
-        console.error(err);
-        return;
-      });
-
-      const svg = await rp({
-        url: `https://xbl-api.prouser123.me/usercolors/define/${encodeURIComponent(
-          JSON.parse(colasset).primaryColor
-        )}/${encodeURIComponent(
-          JSON.parse(colasset).secondaryColor
-        )}/${encodeURIComponent(JSON.parse(colasset).tertiaryColor)}`
-      }).catch(err => {
-        msg.reply("Sorry, I coudn't get your request from the API.");
-        console.error(err);
-        return;
-      });
-
-      const png = await sharp(Buffer.from(svg))
-        .png()
-        .toBuffer();
-
-      msg.channel.send({
-        files: [
-          {
-            name: "colors.png",
-            attachment: png
-          }
-        ]
-      });
+      }
     }
   });
 };
