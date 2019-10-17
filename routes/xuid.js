6e534d8e39b8b114b14fc53eaf395de614f87e84
James Cahill <jcahill773@gmail.com>
2019-10-17 19:11:13 +0100

    Make XUID route async.

--- routes/xuid.js
+++ routes/xuid.js
@@ -1,41 +1,40 @@
 const helpers = require("../helpers");
 const console = require("prefix-logger")("route.xuid");
-const request = require("request");
+const rp = require("request-promise");
 
 module.exports = function(client) {
-  client.on("message", msg => {
+  client.on("message", async msg => {
     if (msg.content.startsWith(helpers.formatCommand("xuid"))) {
       const searchTag = msg.content.split("xuid ")[1];
+      try {
+        const profile = await rp(
+          `https://xbl-api.prouser123.me/profile/xuid/${encodeURIComponent(
+            searchTag
+          )}`
+        );
 
-      if (searchTag.length !== 16 || isNaN(searchTag)) {
-        msg.reply("Invalid XUID. Please try again.");
-        return;
-      }
-
-      const url = `https://xbl-api.prouser123.me/profile/xuid/${encodeURIComponent(
-        searchTag
-      )}`;
-      helpers.debug.level1(`url: ${url}`, "route.xuid");
-
-      request({ url }, function(error, response, body) {
-        // Check the HTTP Response Code.
-        if (response.statusCode !== 200) {
-          msg.reply("Sorry, I coudn't get your request from the API.");
-          console.warn(`HTTP Response ${response.statusCode}`);
+        // Check the profile body for error codes from API.
+        if (!helpers.responseFormatter.checkBody(profile)) {
+          msg.reply("We coudn't find that user. Please try again.");
           return;
         }
 
-        // Check the body for error codes from API.
-        if (!helpers.responseFormatter.checkBody(body)) {
-          msg.reply("We coudn't find that XUID. Please try again.");
-          return;
-        }
+        const fsummary = await rp(
+          `https://xbl-api.prouser123.me/friends/summary/xuid/${encodeURIComponent(
+            searchTag
+          )}`
+        );
 
         // Format data
-        const data = helpers.responseFormatter.px_profile(body);
+        const data = helpers.responseFormatter.px_profile_fsummary(
+          profile,
+          fsummary
+        );
         // Create and send an embed
         helpers.messages.sendRichUserEmbed(client, msg, data);
-      });
+      } catch (err) {
+        return helpers.errorReplyLog(msg, console, err);
+      }
     }
   });
 };
