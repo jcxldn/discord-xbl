1a6523343af2ccfb919394997d68dcd895e1f99e
James Cahill <jcahill773@gmail.com>
2020-04-14 13:46:31 +0100

    Use newer API for usercolors

--- routes/colors.js
+++ routes/colors.js
@@ -9,27 +9,10 @@ module.exports = function(client) {
     if (msg.content.startsWith(helpers.formatCommand("colors"))) {
       const searchTag = msg.content.split("colors ")[1];
       try {
-        const url = `https://xbl-api.prouser123.me/profile/gamertag/${encodeURIComponent(
-          searchTag
-        )}`;
-        helpers.debug.level1(`url: ${url}`, "route.search");
-
-        const user = await rp({
-          url: `https://xbl-api.prouser123.me/profile/gamertag/${encodeURIComponent(
+        const svg = await rp({
+          url: `https://xbl-api.prouser123.me/usercolors/get/gamertag/${encodeURIComponent(
             searchTag
           )}`
-        });
-
-        const colasset = await rp({
-          url: JSON.parse(user).profileUsers[0].settings[9].value
-        });
-
-        const svg = await rp({
-          url: `https://xbl-api.prouser123.me/usercolors/define/${encodeURIComponent(
-            JSON.parse(colasset).primaryColor
-          )}/${encodeURIComponent(
-            JSON.parse(colasset).secondaryColor
-          )}/${encodeURIComponent(JSON.parse(colasset).tertiaryColor)}`
         });
 
         const png = await sharp(Buffer.from(svg))
